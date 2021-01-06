aebe021638b10d0359a577bf5075c8dd2686ffea
sukai <csk199623@icloud.com>
2021-01-07 01:10:46 +0800

    顺序表基本运算的实现

--- SqlList/main.cpp
+++ SqlList/main.cpp
@@ -1,9 +1,19 @@
-#include <bits/stdc++.h>
+
+#include "SqlList.h"
 
 using namespace std;
 
-int main(){
-    
+int main() {
+
+    SqlList* L;
+    int k = 0;
+    int arr[] = {1,2,3,4,5};
+    CreateList(L,arr,5);
+    LocateElem(L,4);
+    ListInsert(L,5,9);
+    ListDelete(L,5,k);
+
+    PrintList(L);
     return 0;
 }
 
